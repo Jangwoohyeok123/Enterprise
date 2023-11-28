16c24e055ae6ffc7f6316f6898346ca36b6f1fa1
Jangwoohyeok123 <dkanvk1@gmail.com>
2023-11-28 18:02:41 +0900

    init- 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { Route } from 'react-router-dom';
+import Footer from './components/common/footer/Footer';
+import Header from './components/common/header/Header';
+import Department from './components/sub/department/Department';
+
 export default function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Header />
+			<Route exact path='/department' component={Department} />
+			<Footer />
+		</div>
+	);
 }
--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,5 +1,5 @@
 import './Department.scss';
 
 export default function Department() {
-	return <div className='Department'>Department</div>;
+	return <></>;
 }
