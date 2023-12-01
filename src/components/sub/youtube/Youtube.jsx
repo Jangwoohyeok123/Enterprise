b0ee24f569799f72f9db398e44227ace9a7848e5
Jangwoohyeok123 <dkanvk1@gmail.com>
2023-12-01 10:16:30 +0900

    .env 파일 설정

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -6,14 +6,13 @@ export default function Youtube() {
 	const [Vids, setVids] = useState([]);
 
 	const fetchYoutube = async () => {
-		const api_key = 'AIzaSyBdiwuRjRbibBfsczkDmMDvmo0PMxc52Uo'; // Cloud 에서 받아옴
-		const playListId = 'PLXHEb7aXbmnWFoUJqvVtMmkRJaNK0i3is'; // youtube url 뒤에 있음
+		const api_key = process.env.REACT_APP_YOUTUBE_API; // Cloud 에서 받아옴
+		const playListId = process.env.REACT_APP_YOUTUBE_API; // youtube url 뒤에 있음
 		const num = 10;
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${playListId}&maxResults=${num}`;
 		const data = await fetch(url);
 		const json = await data.json();
 		setVids(json.items);
-		console.log(Vids);
 	};
 
 	useEffect(() => {
