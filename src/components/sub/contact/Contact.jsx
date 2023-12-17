22b5bac208dd9434f254708b6ea028ce3124bc2c
Jangwoohyeok123 <dkanvk1@gmail.com>
2023-12-17 14:12:50 +0900

    카카오 map 지도 option 설정

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -4,7 +4,9 @@ import emailjs from '@emailjs/browser';
 import './Contact.scss';
 
 export default function Contact() {
-	const kakaoMap = useRef(null);
+	const mapElement = useRef(null);
+	const mapInstance = useRef(null);
+	const marker = useRef(null);
 	// dom 생성후 api 를 호출할 때 필요한 option 을 설정할 때도 api 가 필요함
 
 	const path = useRef(process.env.PUBLIC_URL);
@@ -24,14 +26,51 @@ export default function Contact() {
 		);
 	};
 
+	// naver, google, kakao 위치정보등을 담을 객체
+	// latlng 는 'marker 객체의 Marker 함수의 인자로 전달할 값'
+	// 배열보다는 객체가 인덱싱하기가 편함
+	const mapInfoForInstance = useRef({
+		naver: {
+			title: 'naver',
+			center: new window.kakao.maps.LatLng(37.3589, 127.1052131)
+		},
+		kakao: {
+			title: 'kakako',
+			center: new window.kakao.maps.LatLng(37.55637, 126.92392393)
+		},
+		seoul: {
+			title: 'seoul',
+			center: new window.kakao.maps.LatLng(37.5567, 126.97597)
+		}
+	});
+
+	// kakao api 는 속성의 key 가 center 가 아니면 에러남
 	const mapOption = useRef({
-		center: new window.kakao.maps.LatLng(41.40428934983344, 2.1761382161266485),
+		center: new window.kakao.maps.LatLng(37.5567, 126.97597),
 		level: 3
 	});
 
+	// 카카오 지도를 담을 html 요소와 카카오 지도 객체를 따로 저장하는 구조인가 ?
+	// mapInstance, mapFrame
+	/* 
+		mapInstance 가 한일
+			setCenter info 를 참조하여 setCenter 함
+			new window.kakao.current.maps.Map(mapEl.current, mapOption); 의 결과값임
+	*/
+
+	// 1. markerPostion 생성 marker 가 들어갈 position은 mapOption center 다.
+	// 2. marker 객체 생성 marker 객체를 기존에 등록한 kakao 객체의 Marker 함수를 통해 position 을 전달하면서 생성
+	// 3. 지도에 마커 생성 marker 객체의 지도를 이용해 setMap 함수 호출하면서 지도에 marker 생성
+	// const markerPosition = new kakao.maps.LatLng(33.40123, 244.123123);
+	// const marker = new kakao.maps.Marker({position: markerPosition});
+	// marker.setMap(map);
+	// marker 객체는 지도를 인자로 받아서 setting 하는 함수를 호출한다.
+	// marker 객체는 mapInstance.current 를 인자로 받아 setMap 을 호출한다.
+
 	useEffect(() => {
-		// dom 을 만든 뒤 kakao 객체의 Map 함수를 dom 을 전달하며 호출한다.
-		new window.kakao.maps.Map(kakaoMap.current, mapOption.current);
+		mapInstance.current = new window.kakao.maps.Map(mapElement.current, mapOption.current);
+		marker.current = new window.kakao.maps.Marker({ position: mapInfoForInstance.current['seoul'].center });
+		marker.current.setMap(mapInstance.current);
 	}, []);
 
 	return (
@@ -41,7 +80,7 @@ export default function Contact() {
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 					<br /> Atque nobis hic voluptatibus distinctio!
 				</h3>
-				<article className='mapBox' ref={kakaoMap}></article>
+				<article className='mapBox' ref={mapElement}></article>
 			</header>
 
 			<section className='info'>
@@ -62,15 +101,21 @@ export default function Contact() {
 						</span>
 					</div>
 					<div className='txt-info-footer'>
-						<span>삼성동</span>
-						<span>코엑스</span>
-						<span>엑슨모빌</span>
+						{/* 초기에는 kakako 본사, naver 본사, google 본사 */}
+						{/* 상자를 클릭하면 kakao 본사로 이동하기
+							이동한다란? 
+							1. kakao.Maps(카카오본사 위치)
+							2. marker.setMap(지도) 
+						*/}
+						<span>kakao</span>
+						<span>naver</span>
+						<span>seoul</span>
 					</div>
 				</div>
 
 				<form ref={form} onSubmit={sendEmail}>
 					<div className='profile'>
-						<label for='email_address'>Email Address</label>
+						<label htmlFor='email_address'>Email Address</label>
 						<input
 							type='text'
 							id='email_address'
@@ -78,11 +123,11 @@ export default function Contact() {
 							defaultValue='dkanvk1@gmail.com'
 							placeholder='organizer@gmail.com'
 						/>
-						<label for='name'>Name</label>
+						<label htmlFor='name'>Name</label>
 						<input type='text' id='name' name='user_name' placeholder='name' />
 					</div>
 					<div className='message'>
-						<label for='message'>Message</label>
+						<label htmlFor='message'>Message</label>
 						<textarea name='message' id='message' placeholder='Dear Lorem&#39;s employee' />
 					</div>
 					<input className='submit' type='submit' value='Send' />
@@ -92,4 +137,5 @@ export default function Contact() {
 	);
 }
 
-// dom 요소를 만들고 kakao 에서 제공한 함수에 요소를 인자로 전달한다.
+// dom 요소를 만들고 kakao 에서 제공한 함수에 요소를 인자로 전달한다.\
+// mapInfo center 를 보다 정밀하게 아는 방법
