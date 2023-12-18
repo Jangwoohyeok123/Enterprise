4dc4ad95ee92e14d2d5def946c4f4d27755cd498
Jangwoohyeok123 <dkanvk1@gmail.com>
2023-12-19 08:03:27 +0900

    sign-up reason 제외하고 validation 적용

--- src/components/sub/signup/Signup.jsx
+++ src/components/sub/signup/Signup.jsx
@@ -3,21 +3,23 @@ import { FcGoogle } from 'react-icons/fc';
 import { RiKakaoTalkFill } from 'react-icons/ri';
 import './Signup.scss';
 import { useHistory } from 'react-router-dom';
-import { useRef, useState } from 'react';
-import { useDebounce } from '../../../hooks/useDebounce';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Signup() {
 	const history = useHistory();
 	// reset 시 필요할 수 있기 때문에 useRef 처리한 후 state 할당
 	const initVal = useRef({
-		username: '',
+		userid: '',
 		email: '',
 		password1: '',
 		password2: '',
 		reasons: [] // input checkbox 들은 배열로 관리한다.
 	});
-	const [Val, setVal] = useState(initVal.current);
-	console.log(Val);
+	// Signup page 는 value 를 담을 States 들과 Errors States 를 만들고 시작하자.
+	const [States, setStates] = useState(initVal.current);
+	const [Errors, setErrors] = useState([]);
+
+	// console.log(Errors);
 
 	// const [Inputs, setInputs] = useDebounce(init.current, );
 	// const DebouncedVal = useDebounce(Val);
@@ -27,22 +29,64 @@ export default function Signup() {
 		// const { name, value } = e.target;
 		// setInputs({ ...Inputs, [name]: value });
 		const { name, value } = e.target;
-		setVal({ ...Val, [name]: value });
+		setStates({ ...States, [name]: value });
 	};
 
-	const handleCheck = e => {
+	const handleCheckBox = e => {
 		// 체크할때마다 state 변경하기
 		const { name } = e.target;
 		// e.target 의 부모요소를 가서 input 들 긁어오기
 		const inputs = e.target.parentElement.querySelectorAll('input');
 		const checkArr = []; // check 한 배열을 담을 그릇
-		console.log(inputs);
 		inputs.forEach(input => input.checked && checkArr.push(input.value));
-		setVal({ ...Val, [name]: checkArr });
+		setStates({ ...States, [name]: checkArr });
 	};
-	const handleSubmit = () => {
+
+	const handleSubmit = e => {
+		e.preventDefault();
 		// Register 버튼 클릭했을 때 처리하기
+		// validation 하기, 화면 이동시키기
+		// Errors States 를 분석하고 Error 가 없다면 가입을 승인하고 홈으로 이동시킨다.
+		const errors = validation(States);
+		let flag = true;
+		Array.from(errors).forEach(error => {
+			console.log(error);
+			if (!flag) return;
+			if (error === '') flag = true;
+			else flag = false;
+		});
+
+		flag ? history.push('/') : alert('가입정보를 다시 확인해주세요');
 	};
+
+	const validation = States => {
+		const error = {};
+		// email
+		const [before, after] = States.email.split('@');
+		const afters = after && after.split('.');
+		// 정규표현식
+		const spc = /[!@#$%^&*()[\]_.+]/; // 특수문자 test 용도 표현식
+		const num = /[0-9]/;
+
+		if (States.userid.length < 5) error.userid = '최소 5글자 이상입력하세요';
+		if (!before || !after || !afters[0] || !afters[1]) error.email = '올바른 이메일 형식을 기입하세요!';
+		if (!(States.password1.length < 8) || !spc.test(States.password1) || !num.test(States.password1))
+			error.password1 = '특수문자 및 숫자를 포함하세요.';
+		if (States.password1 !== States.password2 || States.password2.length === 0)
+			error.password2 = '비밀번호가 일치하지 않습니다.';
+		if (States.reasons.length === 0) error.reasons = 'Reason 을 하나이상 체크해주세요';
+		console.log(error);
+
+		return error;
+	};
+
+	// 렌더링이 실행될때 마다 validation 을 진행하라
+	// validation 을 진행한 후 반환한 error 를 Error State 에 저장
+	// Dom 요소에 error 가 있다면 표기하기
+	useEffect(() => {
+		// validation(States);
+		setErrors(validation(States));
+	}, [States]);
 
 	return (
 		<section className='Signup'>
@@ -52,23 +96,32 @@ export default function Signup() {
 				<form onSubmit={handleSubmit}>
 					<section className='text-input'>
 						<div className='text-input-1 input'>
-							<label htmlFor='name'>User Name</label>
-							<input type='text' id='name' placeholder='Enter your Username' onChange={handleChange} />
+							<label htmlFor='userid'>User id {Errors.userid && <strong>({Errors.userid})</strong>}</label>
+							<input name='userid' type='text' id='name' placeholder='Enter your User id' onChange={handleChange} />
 						</div>
 
 						<div className='text-input-2 input'>
-							<label htmlFor='email'>Email</label>
-							<input type='text' id='email' placeholder='Enter your email' onChange={handleChange} />
+							<label htmlFor='email'>Email {Errors.email && <strong>({Errors.email})</strong>}</label>
+							<input name='email' type='text' id='email' placeholder='Enter your email' onChange={handleChange} />
 						</div>
 
 						<div className='text-input-3 input'>
-							<label htmlFor='password'>Password</label>
-							<input type='password' id='password' placeholder='Enter new password' onChange={handleChange} />
+							<label htmlFor='password'>Password {Errors.password1 && <strong>({Errors.password1})</strong>}</label>
+							<input
+								name='password1'
+								type='password'
+								id='password'
+								placeholder='Enter new password'
+								onChange={handleChange}
+							/>
 						</div>
 
 						<div className='text-input-4 input'>
-							<label htmlFor='confirm-password'>Confirm password</label>
+							<label htmlFor='confirm-password'>
+								Confirm password {Errors.password2 && <strong>({Errors.password2})</strong>}
+							</label>
 							<input
+								name='password2'
 								type='password'
 								id='confirm-password'
 								placeholder='Enter confirm password'
@@ -79,18 +132,24 @@ export default function Signup() {
 
 					<section className='else-input'>
 						<div className='reason'>
-							<h3>Sign-up Reason</h3>
+							<h3>Sign-up Reason {Errors.reasons && <strong>({Errors.reasons})</strong>}</h3>
 							<div className='checkboxs'>
-								<input type='checkbox' name='reasons' value='marketing' id='Marketing' onChange={handleCheck} />
+								<input type='checkbox' name='reasons' value='marketing' id='Marketing' onChange={handleCheckBox} />
 								<label htmlFor='Marketing'>Marketing</label>
 
-								<input type='checkbox' name='reasons' value='trends' id='Market Trends' onChange={handleCheck} />
+								<input type='checkbox' name='reasons' value='trends' id='Market Trends' onChange={handleCheckBox} />
 								<label htmlFor='Market Trends'>Market Trends</label>
 
-								<input type='checkbox' name='reasons' value='collaboration' id='Collaboration' onChange={handleCheck} />
+								<input
+									type='checkbox'
+									name='reasons'
+									value='collaboration'
+									id='Collaboration'
+									onChange={handleCheckBox}
+								/>
 								<label htmlFor='Collaboration'>Collaboration</label>
 
-								<input type='checkbox' name='reasons' value='others' id='Sponsorship' onChange={handleCheck} />
+								<input type='checkbox' name='reasons' value='others' id='Sponsorship' onChange={handleCheckBox} />
 								<label htmlFor='others'>others</label>
 							</div>
 						</div>
@@ -127,4 +186,8 @@ export default function Signup() {
 	val state 가 변경될 때마다 setTimeout 이 초기화 된 후 다시 할당된다.
 
 	내가 궁금한 점은 val 가 변경될때마다 setState 호출을 막으려고 디바운싱을 하는건데 이러면 안되는거 아님? 
+*/
+
+/* 
+	onChange 가 발생할 때 States 가 변경되지 않음
 */
