8cb0f39a2430019168dd56c4b401392e481e57a4
Jangwoohyeok123 <dkanvk1@gmail.com>
2023-12-21 09:27:49 +0900

    redux-saga 설명 App.js 에 주석으로 기입

--- src/App.jsx
+++ src/App.jsx
@@ -41,4 +41,21 @@ export default function App() {
 		</div>
 	);
 }
-//signup page 에서는
+
+/* redux-saga 에 관하여..
+
+fetch 파일
+=> action 객체를 인터셉트 한 후 saga 가 처리할 fetch 로직들을 담은 파일
+
+reducers 파일
+=> 전역 state 를 담당하는 reducer 들을 모아둔 파일 action 타입에 따라서 state 의 변경을 진행한다. 
+
+sagas 파일 
+=> component 가 dispatch 한 action 을 인터셉트 한 후 별도의 처리를 한 후 reducer 에게 스스로 만든 action 객체를 전달하는 역할을 함
+
+actionTables 파일
+=> action 들을 한 파일로 모아둠 컴포넌트나 리듀서는 이 파일을 보면서 dispatch 할 때, reducer 는 type 에 따른 작업을 정의할 때 보면서 작업하자
+
+store 파일 
+=> redux 전역 store 파일을 만드는 공간으로 Provider 이 파일에서 생성한 store 를 전달하면 App component 는 전역 state 를 useSelector 를 통해 사용가능하다.
+*/
