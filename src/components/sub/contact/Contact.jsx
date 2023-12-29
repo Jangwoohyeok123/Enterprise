c0dd52cc21a193db63ab42b6beecd1866b7c75d6
Jangwoohyeok123 <dkanvk1@gmail.com>
2023-12-29 18:08:09 +0900

    contact page

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,95 +1,169 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import Layout from '../../common/layout/Layout';
-import emailjs from '@emailjs/browser';
 import './Contact.scss';
+import emailjs from '@emailjs/browser';
+import useThrottle from '../../../hooks/useThrottle';
 
 export default function Contact() {
-	const kakaoMap = useRef(null);
-	// dom 생성후 api 를 호출할 때 필요한 option 을 설정할 때도 api 가 필요함
-
-	const path = useRef(process.env.PUBLIC_URL);
-
-	// emailJS
+	console.log('contact');
 	const form = useRef();
+	const resetForm = () => {
+		const elArr = form.current.children;
+
+		Array.from(elArr).forEach(el => {
+			console.log(el);
+			if (el.name === 'user_name' || el.name === 'user_email' || el.name === 'message') el.value = '';
+		});
+	};
 	const sendEmail = e => {
 		e.preventDefault();
 
-		emailjs.sendForm('service_vj48mbo', 'template_j2mozwh', form.current, 'x-s69WH3OOeOCRhr0').then(
+		const [user, email] = form.current.querySelectorAll('input');
+		const txtArea = form.current.querySelector('textarea');
+
+		if (!user.value || !email.value || !txtArea.value)
+			return alert('이름, 답장받을 이메일주소 문의내용을 모두 입력하세요.');
+
+		emailjs.sendForm('service_ag7z96s', 'template_oh9ajns', form.current, '23g8RepczesqKPoIX').then(
 			result => {
-				console.log(result.text);
+				alert('문의 내용이 성공적으로 전송되었습니다.');
+				resetForm();
 			},
 			error => {
-				console.log(error.text);
+				alert('일시적인 장애로 문의 전송에 실패했습니다. 다음의 메일주소로 보내주세요.');
+				resetForm();
 			}
 		);
 	};
 
-	const mapOption = useRef({
-		center: new window.kakao.maps.LatLng(41.40428934983344, 2.1761382161266485),
-		level: 3
+	const kakao = useRef(window.kakao);
+	const [Index, setIndex] = useState(0);
+	const [Traffic, setTraffic] = useState(false);
+	const [View, setView] = useState(false);
+
+	const mapFrame = useRef(null);
+	const viewFrame = useRef(null);
+	const marker = useRef(null);
+	const mapInstance = useRef(null);
+
+	const mapInfo = useRef([
+		{
+			title: '삼성역 코엑스',
+			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
+		},
+		{
+			title: '넥슨 본사',
+			latlng: new kakao.current.maps.LatLng(37.40211707077346, 127.10344953763003),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
+		},
+		{
+			title: '서울 시청',
+			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) }
+		}
+	]);
+
+	marker.current = new kakao.current.maps.Marker({
+		position: mapInfo.current[Index].latlng,
+		image: new kakao.current.maps.MarkerImage(
+			mapInfo.current[Index].imgSrc,
+			mapInfo.current[Index].imgSize,
+			mapInfo.current[Index].imgOpt
+		)
 	});
 
+	const roadview = useCallback(() => {
+		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, panoId => {
+			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
+		});
+	}, [Index]);
+
+	const setCenter = useCallback(() => {
+		console.log('setCenter');
+		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
+		//roadview.current();
+	}, [Index]);
+
+	const throttledSetCenter = useThrottle(setCenter);
+
 	useEffect(() => {
-		// dom 을 만든 뒤 kakao 객체의 Map 함수를 dom 을 전달하며 호출한다.
-		new window.kakao.maps.Map(kakaoMap.current, mapOption.current);
-	}, []);
+		//Index값이 변경되는 것은 출력할 맵정보가 변경된다는 의미이므로 기존 프레임 안쪽의 정보를 지워서 초기화
+		mapFrame.current.innerHTML = '';
+		viewFrame.current.innerHTML = '';
+		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
+			center: mapInfo.current[Index].latlng,
+			level: 3
+		});
+		marker.current.setMap(mapInstance.current);
+		setTraffic(false);
+		setView(false);
+
+		mapInstance.current.addControl(
+			new kakao.current.maps.MapTypeControl(),
+			kakao.current.maps.ControlPosition.TOPRIGHT
+		);
+		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
+		mapInstance.current.setZoomable(false);
+	}, [Index]);
+
+	//윈도우 객체에 등록할 핸들러함수를 따로 useEffect로 빼놓은 다음 의존성 배열을 가급적 비워두는 것이 좋음
+	//윈도우 등록되는 핸들러함수에 만약 특정 state값을 의존한다면은 연결되는 함수이름자체를 의존성배열에 등록하되 useCallback처리
+	useEffect(() => {
+		window.addEventListener('resize', throttledSetCenter);
+		return () => window.removeEventListener('resize', throttledSetCenter);
+	}, [throttledSetCenter]);
+
+	useEffect(() => {
+		Traffic
+			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
+			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
+
+	useEffect(() => {
+		View && viewFrame.current.children.length === 0 && roadview();
+	}, [View, roadview]);
 
 	return (
-		<Layout title={'Contact Us'} className={'Contact'}>
-			<header>
-				<h3 style={{ fontSize: '1.5rem' }}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					<br /> Atque nobis hic voluptatibus distinctio!
-				</h3>
-				<article className='mapBox' ref={kakaoMap}></article>
-			</header>
-
-			<section className='info'>
-				<div className='txt-info'>
-					<div className='txt-info-header'>
-						<span>LOCATION</span>
-						<h2 style={{ fontSize: '2rem' }}>HOW TO FIND US</h2>
-					</div>
-					<div className='txt-info-body'>
-						<span>
-							<strong>Address</strong> = {'map요소'}
-						</span>
-						<span>
-							<strong>Telephone</strong> = {'map요소'}
-						</span>
-						<span>
-							<strong>Fax</strong> = {'map요소'}
-						</span>
-					</div>
-					<div className='txt-info-footer'>
-						<span>삼성동</span>
-						<span>코엑스</span>
-						<span>엑슨모빌</span>
-					</div>
-				</div>
-
+		<Layout title={'Contact'}>
+			<div id='mailSection'>
 				<form ref={form} onSubmit={sendEmail}>
-					<div className='profile'>
-						<label for='email_address'>Email Address</label>
-						<input
-							type='text'
-							id='email_address'
-							name='to_email'
-							defaultValue='dkanvk1@gmail.com'
-							placeholder='organizer@gmail.com'
-						/>
-						<label for='name'>Name</label>
-						<input type='text' id='name' name='user_name' placeholder='name' />
-					</div>
-					<div className='message'>
-						<label for='message'>Message</label>
-						<textarea name='message' id='message' placeholder='Dear Lorem&#39;s employee' />
-					</div>
-					<input className='submit' type='submit' value='Send' />
+					<label>Name</label>
+					<input type='text' name='user_name' />
+					<label>Email</label>
+					<input type='email' name='user_email' />
+					<label>Message</label>
+					<textarea name='message' />
+					<input type='submit' value='Send' />
 				</form>
-			</section>
+			</div>
+
+			<div id='mapSection'>
+				<div className='controlBox'>
+					<nav className='branch'>
+						{mapInfo.current.map((el, idx) =>
+							//prettier-ignore
+							<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>{el.title}</button>
+						)}
+					</nav>
+
+					<nav className='info'>
+						<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
+						<button onClick={() => setView(!View)}>{View ? 'map' : 'road view'}</button>
+						<button onClick={setCenter}>위치 초기화</button>
+					</nav>
+				</div>
+				<section className='tab'>
+					<article className={`mapBox ${View ? '' : 'on'}`} ref={mapFrame}></article>
+					<article className={`viewBox ${View ? 'on' : ''}`} ref={viewFrame}></article>
+				</section>
+			</div>
 		</Layout>
 	);
 }
-
-// dom 요소를 만들고 kakao 에서 제공한 함수에 요소를 인자로 전달한다.
