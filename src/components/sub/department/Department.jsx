e62a703214d92b8003abb79b4a6e5dafdfe6c3f8
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-02 20:10:36 +0900

    department 수정

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -6,18 +6,22 @@ import Layout from '../../common/layout/Layout';
 import { useCookie } from '../../../hooks/useCookie';
 
 export default function Department() {
-	const { data: json, isSuccess: isDepartment } = useQueryDepartment();
+	const { data: json, isSuccess: isDepartment } =
+		useQueryDepartment();
 	const path = useRef(process.env.PUBLIC_URL);
 	const splitHeader = useTextMethod('split');
 	const createCookie = useCookie();
 
 	return (
-		<Layout className='Department' title={'NORM ARCHITECTS STUDIO WAS FOUNDED IN 2008'}>
+		<Layout
+			className='Department'
+			title={'NORM ARCHITECTS STUDIO WAS FOUNDED IN 2008'}>
 			{isDepartment && (
 				<>
 					<section className='Department-firstSection'>
 						<div className='header'>
 							<img
+								className='mainImage'
 								src={`${path.current}/img/department/${json.article.mainImg}`}
 								alt='mainImage'
 								onClick={() => {
@@ -31,27 +35,36 @@ export default function Department() {
 							<div>
 								<p>{json.article.body}</p>
 								<div className='cards'>
-									{json.article.features.map((feature, idx) => {
-										return (
-											<div className='card' key={feature + idx}>
-												<h4>{feature.header}</h4>
-												<p>{feature.body}</p>
-											</div>
-										);
-									})}
+									{json.article.features.map(
+										(feature, idx) => {
+											return (
+												<div
+													className='card'
+													key={feature + idx}>
+													<h4>{feature.header}</h4>
+													<p>{feature.body}</p>
+												</div>
+											);
+										}
+									)}
 								</div>
 							</div>
 						</div>
 					</section>
 					<section className='Department-secondSection'>
-						{splitHeader('norm-people', true).map((title, idx) => {
-							return <h3 key={title + idx}>{title}</h3>;
-						})}
+						{splitHeader('norm-people', true).map(
+							(title, idx) => {
+								return <h3 key={title + idx}>{title}</h3>;
+							}
+						)}
 						<div className='container'>
 							{json['norm-people'].map((member, idx) => {
 								return (
 									<div className='card' key={member + idx}>
-										<img src={`${path.current}/img/department/${member.pic}`} alt='memberImage' />
+										<img
+											src={`${path.current}/img/department/${member.pic}`}
+											alt='memberImage'
+										/>
 										<div className='text'>
 											<h5>{member.name}</h5>
 											<p>{member.position}</p>
@@ -62,15 +75,19 @@ export default function Department() {
 						</div>
 					</section>
 					<section className='Department-thirdSection'>
-						{splitHeader('our-awards', true).map((title, idx) => {
-							return <h3 key={title + idx}>{title}</h3>;
-						})}
+						{splitHeader('our-awards', true).map(
+							(title, idx) => {
+								return <h3 key={title + idx}>{title}</h3>;
+							}
+						)}
 						<div className='awards-table'>
 							<thead>
 								<tr>
-									{json['our-awards'].header.map((col, idx) => {
-										return <th key={col + idx}>{col}</th>;
-									})}
+									{json['our-awards'].header.map(
+										(col, idx) => {
+											return <th key={col + idx}>{col}</th>;
+										}
+									)}
 								</tr>
 							</thead>
 							<tbody>
@@ -78,7 +95,9 @@ export default function Department() {
 									return (
 										<tr key={row + idx}>
 											{row.map((data, idx) => {
-												return <td key={data + idx}>{data}</td>;
+												return (
+													<td key={data + idx}>{data}</td>
+												);
 											})}
 										</tr>
 									);
