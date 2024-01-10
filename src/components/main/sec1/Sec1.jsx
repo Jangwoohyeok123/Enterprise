b8df76ce72ec8f9a88badcdabfb3167d4fe8f746
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-11 01:34:32 +0900

    main page section 1 완성

--- src/components/main/sec1/Sec1.jsx
+++ src/components/main/sec1/Sec1.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { useScroll } from '../../../hooks/useScroll';
 import Image from '../../common/img/Image';
 import './Sec1.scss';
 
 export default function Sec1() {
 	const handleScroll = modifiedScroll => {};
+	const [SecArr, setSecArr] = useState([1, 2, 3]);
 
 	const { refEl } = useScroll(handleScroll);
 	const path = process.env.PUBLIC_URL;
@@ -11,72 +13,38 @@ export default function Sec1() {
 	return (
 		<section className='Sec1 myScroll'>
 			<div className='wrap'>
-				<div className={`item${1}`}>
-					<div className='left'>
-						<div className='subtit'>LOR SERVICES</div>
-						<Image className='sec1Img' src={`${path}/img/main/pic${1}.jpg`} />
-					</div>
-					<div className='right'>
-						<div className='numbering'>
-							<h2>0{1}</h2>
-							<span>/ DEPARTMENT</span>
+				<div className='subtit1'>NORM SERVICES</div>
+				<div className='subtit2'>LSOSERVIESSDAMPJALL</div>
+				{SecArr.map((_, idx) => {
+					const number = idx + 1;
+					return (
+						<div className={`item`}>
+							<div className='img'>
+								<Image
+									className='sec1Img'
+									src={`${path}/img/main/pic${number}.jpg`}
+								/>
+							</div>
+							<div className='txt'>
+								<div className='numbering'>
+									<h2>0{number}</h2>
+									<span>/ DEPARTMENT</span>
+								</div>
+								<div className='intro'>
+									<h3>Construction Building</h3>
+									<p>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit.
+										Quaerat aspernatur distinctio quas. Eos perspiciatis facere
+										aut dolorem quam corrupti aliquid, dolorum, saepe quas
+										quaerat beatae qui? Reiciendis id nam sint! Lorem ipsum
+										dolor sit amet.
+									</p>
+								</div>
+								<span className='to'>DEPARTMENT</span>
+							</div>
 						</div>
-						<div className='intro'>
-							<h3>Construction Building</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-								aspernatur distinctio quas. Eos perspiciatis facere aut dolorem
-								quam corrupti aliquid, dolorum, saepe quas quaerat beatae qui?
-								Reiciendis id nam sint! Lorem ipsum dolor sit amet.
-							</p>
-						</div>
-						<span className='to'>DEPARTMENT</span>
-					</div>
-				</div>
-				<div className={`item${2}`}>
-					<div className='left'>
-						<div className='subtit'>LOR SERVICES</div>
-						<Image className='sec1Img' src={`${path}/img/main/pic${2}.jpg`} />
-					</div>
-					<div className='right'>
-						<div className='numbering'>
-							<h2>0{2}</h2>
-							<span>/ DEPARTMENT</span>
-						</div>
-						<div className='intro'>
-							<h3>Construction Building</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-								aspernatur distinctio quas. Eos perspiciatis facere aut dolorem
-								quam corrupti aliquid, dolorum, saepe quas quaerat beatae qui?
-								Reiciendis id nam sint! Lorem ipsum dolor sit amet.
-							</p>
-						</div>
-						<span className='to'>DEPARTMENT</span>
-					</div>
-				</div>
-				<div className={`item${3}`}>
-					<div className='left'>
-						<div className='subtit'>LOR SERVICES</div>
-						<Image className='sec1Img' src={`${path}/img/main/pic${3}.jpg`} />
-					</div>
-					<div className='right'>
-						<div className='numbering'>
-							<h2>0{3}</h2>
-							<span>/ DEPARTMENT</span>
-						</div>
-						<div className='intro'>
-							<h3>Construction Building</h3>
-							<p>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-								aspernatur distinctio quas. Eos perspiciatis facere aut dolorem
-								quam corrupti aliquid, dolorum, saepe quas quaerat beatae qui?
-								Reiciendis id nam sint! Lorem ipsum dolor sit amet.
-							</p>
-						</div>
-						<span className='to'>DEPARTMENT</span>
-					</div>
-				</div>
+					);
+				})}
 			</div>
 		</section>
 	);
