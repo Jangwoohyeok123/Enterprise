c92c0f294b434f7dafd67eef82133cc57651fee9
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-10 17:33:12 +0900

    3d visual component layout temp

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,6 +1,5 @@
 import './Visual.scss';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -10,38 +9,18 @@ import { useQueryYoutube } from '../../../query/useQueryYoutube';
 import { useEffect } from 'react';
 
 export default function Visual() {
-	const num = useRef(3); // 몇 개의 사진을 루프돌릴건지를 정하는 변수
-	const swipeRef = useRef(null);
 	const { isSuccess, data } = useQueryYoutube();
-	const [PrevIndex, setPrevIndex] = useState(0);
-	const [Index, setIndex] = useState(0);
-	const [NextIndex, setNextIndex] = useState(0);
-
-	const swiperOpt = useRef({
-		modules: [Autoplay],
-		loop: true,
-		slidesPerView: 1,
-		spaceBetween: 50,
-		centeredSlides: true,
-		loopedSlides: num.current,
-		onSwiper: swiper => {
-			swipeRef.current = swiper;
-		},
-		onSlideChange: swiper => {
-			setIndex(swiper.realIndex);
-			swiper.realIndex === 0
-				? setPrevIndex(num.current - 1)
-				: setPrevIndex(swiper.realIndex - 1);
-			swiper.realIndex === num.current - 1
-				? setNextIndex(0)
-				: setNextIndex(swiper.realIndex + 1);
-		},
-		autoplay: { delay: 2000, disableOnInteraction: true },
-		breakpoints: {
-			1050: { slidesPerView: 2 },
-			1400: { slidesPerView: 3 }
-		}
-	});
+	const path = useRef(process.env.PUBLIC_URL);
+	const arr = useRef([
+		'Best Performance',
+		'Lorem Current',
+		'Road Racer',
+		'Hello Editor',
+		"What's new",
+		'Lorem ipsum',
+		'Go away',
+		'Animal Hanlder'
+	]);
 
 	const trimTitle = title => {
 		let resultTit = '';
@@ -51,96 +30,31 @@ export default function Visual() {
 		return resultTit;
 	};
 
-	useEffect(() => {}, []);
-	// modules={[Navigation, Pagination]} slidesPerView={3}
-
 	return (
 		<figure className='Visual myScroll'>
-			<div className='txtBox'>
-				<ul>
-					{isSuccess &&
-						data.map((el, idx) => {
-							if (idx >= num.current) return null;
-
-							return (
-								<li key={el.id} className={idx === Index ? 'on' : ''}>
-									<h3>{trimTitle(el.snippet.title)}</h3>
-								</li>
-							);
-						})}
-				</ul>
-			</div>
-
-			<Swiper {...swiperOpt.current}>
+			<video
+				src={`${path.current}/img/main/mainVideo.mp4`}
+				loop
+				autoPlay
+				muted></video>
+			<section id='circle'>
 				{isSuccess &&
 					data.map((el, idx) => {
-						if (idx >= num.current) return null;
 						return (
-							<SwiperSlide key={el.id}>
-								<div className='pic'>
-									<p>
+							<article className={`face${idx + 1}`}>
+								<h1>{arr.current[idx]}</h1>
+								<div className='inner'>
+									<div className='imgBox'>
 										<img
 											src={el.snippet.thumbnails.standard.url}
 											alt={el.snippet.title}
 										/>
-									</p>
-									<p>
-										<img
-											src={el.snippet.thumbnails.standard.url}
-											alt={el.snippet.title}
-										/>
-									</p>
+									</div>
 								</div>
-							</SwiperSlide>
+							</article>
 						);
 					})}
-			</Swiper>
-
-			<nav className='preview'>
-				{isSuccess && (
-					<>
-						<p
-							className='prevBox'
-							onClick={() => swipeRef.current.slidePrev(400)}>
-							<img
-								src={data[PrevIndex].snippet.thumbnails.default.url}
-								alt={data[PrevIndex].snippet.title}
-							/>
-						</p>
-						<p
-							className='nextBox'
-							onClick={() => swipeRef.current.slideNext(400)}>
-							<img
-								src={data[NextIndex].snippet.thumbnails.default.url}
-								alt={data[NextIndex].snippet.title}
-							/>
-						</p>
-					</>
-				)}
-			</nav>
-
-			<ul className='pagination'>
-				{Array(num.current)
-					.fill()
-					.map((_, idx) => {
-						return (
-							<li
-								key={idx}
-								className={idx === Index ? 'on' : ''}
-								onClick={() => swipeRef.current.slideToLoop(idx, 400)}></li>
-						);
-					})}
-			</ul>
-
-			<div className='barFrame'>
-				<p
-					className='bar'
-					style={{ width: (100 / num.current) * (Index + 1) + '%' }}></p>
-			</div>
-
-			<div className='counter'>
-				<strong>0{Index + 1}</strong>/<span>0{num.current}</span>
-			</div>
+			</section>
 		</figure>
 	);
 }
