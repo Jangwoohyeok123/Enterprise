b21206cd7bbff2901061a0e93c6660b153c76e3a
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-16 07:23:05 +0900

    partners motion 작업했으나 수정 필요

--- src/components/sub/youtube/comp/partners/Partners.jsx
+++ src/components/sub/youtube/comp/partners/Partners.jsx
@@ -1,16 +1,38 @@
+import { useEffect, useRef } from 'react';
 import './Partners.scss';
 
 export default function Partners() {
+	const pointer = useRef(null);
+	const youtubers = [
+		'Crystoper',
+		'Author',
+		'Kim lim',
+		'Alexander',
+		'Author Kirem',
+		'Soap people',
+		'Hizacky'
+	];
+
+	const move = e => {
+		const targetPosition = e.target.offsetTop - 25;
+		console.log(targetPosition);
+		pointer.current.style.top = targetPosition + 'px';
+		e.target.style.fontWeight = 600;
+	};
+
 	return (
 		<div className='Partners'>
-			<span>Crystoper</span>
-			<span>Author</span>
-			<span>Author</span>
-			<span>Author</span>
-			<span>Author Kirem</span>
-			<span>Author</span>
-			<span>CatDoru</span>
-			<div className='card'>&ABDGO</div>
+			{youtubers.map((youtuber, idx) => {
+				return (
+					<span key={idx} onClick={move}>
+						{youtuber}
+					</span>
+				);
+			})}
+
+			<div className='card' ref={pointer}>
+				&ABDGO
+			</div>
 		</div>
 	);
 }
