23cf61850c69f4d019f40c58b3020605526a3025
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-16 19:24:27 +0900

    card component 를 Video, Description 으로 분리

--- src/components/sub/youtube/comp/sec2/card/Description.jsx
+++ src/components/sub/youtube/comp/sec2/card/Description.jsx
@@ -1,5 +1,18 @@
+import useTextMethod from '../../../../../../hooks/useText';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { IoArrowForwardCircleOutline } from 'react-icons/io5';
 import './Description.scss';
 
-export default function Description() {
-	return <div className='Description'>Description</div>;
+export default function Description({ video }) {
+	const charSlice = useTextMethod('charSlice');
+
+	return (
+		<div className='description'>
+			<p>{charSlice(video.snippet.description, 190)} ...</p>
+			<Link className='more' to={`/detail/${video.id}`}>
+				<span>more</span>
+				<IoArrowForwardCircleOutline className='icon' />
+			</Link>
+		</div>
+	);
 }
