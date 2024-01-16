3de22925eb0aa2b30e4f4ad7181bcd3c1ef63968
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-16 17:03:44 +0900

    youtube page font 상위 컴포넌트로 집합해 처리

--- src/components/sub/youtube/comp/partners/Partners.jsx
+++ src/components/sub/youtube/comp/partners/Partners.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './Partners.scss';
 
 export default function Partners() {
 	const pointer = useRef(null);
-	const youtubers = [
+	const [Youtubers, setYoutubers] = useState([
 		'Crystoper',
 		'Author',
 		'Kim lim',
@@ -11,20 +11,35 @@ export default function Partners() {
 		'Author Kirem',
 		'Soap people',
 		'Hizacky'
-	];
+	]);
 
-	const move = e => {
-		const targetPosition = e.target.offsetTop - 25;
-		console.log(targetPosition);
-		pointer.current.style.top = targetPosition + 'px';
-		e.target.style.fontWeight = 600;
+	// 부모를 찾는 방법은 parentElement
+	// Partners 에서 span list 만들기
+	// click span 에 on 붙이기
+	const move = (e, idx) => {
+    // Nodelist
+		const spanList2 = e.target.parentElement.querySelectorAll('span');
+
+    // list 
+		const spanList = Array.from(
+			e.target.parentElement.querySelectorAll('span')
+		);
+
+		console.log(spanList2);
+		console.log(spanList);
+		spanList.map((span, idx) => {
+			span.classList.remove('on');
+		});
+
+		console.log(spanList[idx].classList.add('on'));
+		// spanList[idx].classList.add('on');
 	};
 
 	return (
 		<div className='Partners'>
-			{youtubers.map((youtuber, idx) => {
+			{Youtubers.map((youtuber, idx) => {
 				return (
-					<span key={idx} onClick={move}>
+					<span key={idx} onClick={e => move(e, idx)}>
 						{youtuber}
 					</span>
 				);
@@ -36,3 +51,11 @@ export default function Partners() {
 		</div>
 	);
 }
+// 클릭할 때마다
+
+/* 
+	const targetPosition = e.target.offsetTop - 25;
+	console.log(targetPosition);
+	pointer.current.style.top = targetPosition + 'px';
+	e.target.style.fontWeight = 600;
+*/
