7f5a8064fb30e072b6a62ce8a1dbffc4ea6a4f3e
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-18 15:48:32 +0900

    Branch button

--- src/components/sub/contact/components/Branch.jsx
+++ src/components/sub/contact/components/Branch.jsx
@@ -1,11 +1,17 @@
 import './Branch.scss';
 
 export default function Branch({ mapInfo, setIndex, Index }) {
+	const colors = {
+		seoul: '#007bff',
+		naver: '#03c75a',
+		kakao: '#ffcd00'
+	};
+	//colors[el.title]
 	return (
 		<nav className='Branch'>
 			{mapInfo.current.map((el, idx) =>
 				//prettier-ignore
-				<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>{el.title}</button>
+				<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''} style={ idx === Index ? {background: colors[el.title] } : {}}>{el.title}</button>
 			)}
 		</nav>
 	);
