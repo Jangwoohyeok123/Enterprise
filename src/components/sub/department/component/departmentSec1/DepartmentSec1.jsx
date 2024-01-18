794c55aa19da82b377e38ffabeb1fe8b412b7858
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-18 21:43:09 +0900

    department divide 전

---
+++ src/components/sub/department/component/departmentSec1/DepartmentSec1.jsx
@@ -0,0 +1,5 @@
+import './DepartmentSec1.scss';
+
+export default function DepartmentSec1() {
+	return <div className='DepartmentSec1'>DepartmentSec1</div>;
+}
---
+++ src/components/sub/department/component/departmentSec2/DepartmentSec2.jsx
@@ -0,0 +1,5 @@
+import './DepartmentSec2.scss';
+
+export default function DepartmentSec2() {
+	return <div className='DepartmentSec2'>DepartmentSec2</div>;
+}
