820c1ce56fb5a4066b3dcd6f8c27fde1821d7d3f
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-20 12:01:29 +0900

    department sec1 처리

--- src/components/sub/department/component/departmentSec1/DepartmentSec1.jsx
+++ src/components/sub/department/component/departmentSec1/DepartmentSec1.jsx
@@ -4,9 +4,9 @@ export default function DepartmentSec1({ json }) {
 	return (
 		<section className='DepartmentSec1'>
 			<div className='sec1Container'>
-				<h2>{json.article.subHeadline}</h2>
-				<div>
-					<p>{json.article.body}</p>
+				<h2 className='left'>{json.article.subHeadline}</h2>
+				<div className='right'>
+					<p className='body'>{json.article.body}</p>
 					<div className='cards'>
 						{json.article.features.map((feature, idx) => {
 							return (
