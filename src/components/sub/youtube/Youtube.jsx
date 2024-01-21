8153bde8c45d3ab4382f7c46be74c2bc981a0ce7
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-21 11:43:49 +0900

    Youtube Service component 생성

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,24 +1,22 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
 import { useQueryYoutube } from '../../../query/useQueryYoutube';
 import Card from './comp/card/Card';
+import Service from './comp/service/Service';
 
 export default function Youtube() {
 	// component 분리하기
 	const path = useRef(process.env.PUBLIC_URL);
-	const { data: Vids, isSuccess, isLoading, isError } = useQueryYoutube();
+	const { data: Vids, isSuccess } = useQueryYoutube();
 
 	return (
 		<Layout
 			title={'Youtube'}
 			src={`${path.current}/img/youtube/main.jpg`}
 			className='Youtube'>
+			<Service />
 			<section className='Youtube-Section'>
-				<div className='header'>
-					<h2>Working Videos</h2>
-					<h3>Description</h3>
-				</div>
 				{isSuccess &&
 					Vids.map((video, idx) => {
 						return <Card video={video} idx={idx} />;
@@ -27,21 +25,3 @@ export default function Youtube() {
 		</Layout>
 	);
 }
-
-/* 
-<section className='introduce'>
-				We are ready <strong>5 projects</strong> videos for you
-			</section>
-			<section className='articles'>
-				
-			</section>
-
-			const [date, tmpTime] = video.snippet.publishedAt.split('T');
-			wordSlice(video.snippet.title, 5)
-			wordSlice(video.snippet.description, 50)
-			video.snippet.thumbnails.standard.url} alt={video.snippet.title}
-*/
-
-/* 
-  home 을 갔다가 Youtube 로 가면 문제가 없는데 youtube 로 바로가면 문제가 생김
-*/
---
+++ src/components/sub/youtube/comp/service/Service.jsx
@@ -0,0 +1,9 @@
+import './Service.scss';
+
+export default function Service() {
+	return (
+		<figure className='Service'>
+			<div className='service-title'>Working Videos</div>
+		</figure>
+	);
+}
