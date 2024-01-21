6b9e9fdbecac9e078e64feae4ad48c928125818b
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-21 12:07:36 +0900

    service 추가

--- src/components/sub/youtube/comp/service/Service.jsx
+++ src/components/sub/youtube/comp/service/Service.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './Service.scss';
 
 export default function Service() {
+	const [Categorys, setCategorys] = useState([]);
+
 	return (
 		<figure className='Service'>
-			<div className='service-title'>Working Videos</div>
+			<div className='Service-title'>Working Vides & Description</div>
+			<div className='category'>
+				<span className='sports'>Sports</span>
+				<span className='games'>Game</span>
+				<span className='asmrs'>Asmr</span>
+				<span className='citys'>City</span>
+			</div>
 		</figure>
 	);
 }
