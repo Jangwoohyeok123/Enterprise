9556b956db19cd3add4acbec4a6edcd8258c071b
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-01-28 23:00:58 +0900

    포폴 잔잔바리 정리 main page 의 useTargetScroll 과 signup page layout 정리, community edit 함수 수정안하고 수정누른경우, gallery 페이지 Tab 과 Search bar 의 반응형 처리 필요 - 2

--- src/hooks/useGlobalData.js
+++ src/hooks/useGlobalData.js
@@ -26,14 +26,24 @@ export function GlobalProvider({ children }) {
 	);
 }
 
-// GlobalContext 안에 선언됐던 value 를 전달해주는 훅이다.
-// 훅 사용자는 const {state, modifyFc} = useGlobalData();
 export function useGlobalData() {
 	const globalData = useContext(GlobalContext);
 	return globalData;
 }
 
-/* 
+// GlobalContext 안에 선언됐던 value 를 전달해주는 훅이다.
+// 훅 사용자는 const {state, modifyFc} = useGlobalData();
 
+/* 
+	context API 는 아래의 과정이 필요하다.
+
+	1. createContext 호출 
+	2. 전역 state 선언
+	3. provider 에게 전달
+
+	[ 사용법 ]
+
+	useContext 를 통해 provider 에게 전달할 데이터를 받아서 사용할 수 있다.
+	ex) const 전역데이터 =  useContext(GlobalContext); // Context 도 여러개 선언할 수 있음 
 
 */
